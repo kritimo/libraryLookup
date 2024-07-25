f0164fb51f54349c2ffdb4eb93c7efd863caf09b
Kriti Moogala <kmoogala@umich.edu>
2024-07-25 12:34:15 -0400

    basic book functionalities

--- book.cpp
+++ book.cpp
@@ -6,3 +6,30 @@
 //
 
 #include "book.hpp"
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+Book::Book(std::string title, std::string author, std::string isbn, std::string genre)
+    : title(title), author(author), isbn(isbn), genre(genre), available(true) {}
+
+string Book::getTitle() { return title; }
+string Book::getAuthor() { return author; }
+string Book::getISBN() { return isbn; }
+string Book::getGenre() { return genre;}
+bool Book::isAvailable() { return available; }
+
+void Book::setAvailable(bool availability) {
+    available = availability;
+}
+
+void Book::displayDetails() {
+    cout << "Title: " << title << endl;
+    cout << "Author: " << author << endl;
+    cout << "ISBN: " << isbn << endl;
+    cout << "Available: " << (available ? "Yes" : "No") << endl;
+    cout << endl;
+}
+
--- book.hpp
+++ book.hpp
@@ -7,7 +7,28 @@
 
 #ifndef book_hpp
 #define book_hpp
+#include <string>
 
-#include <stdio.h>
+using namespace std;
+
+class Book {
+private:
+    string title;
+    string author;
+    string isbn;
+    string genre;
+    bool available;
+    
+
+public:
+    Book(string title, string author, string isbn, string genre);
+    string getTitle();
+    string getAuthor();
+    string getISBN();
+    string getGenre();
+    bool isAvailable();
+    void setAvailable(bool availability);
+    void displayDetails();
+};
 
 #endif /* book_hpp */
